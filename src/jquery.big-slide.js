10aa5fa12360715bde41a7880f69d5bbd1ed555c
darth-cheney <eric.gade@gmail.com>
2014-01-21 19:11:13 -0500

    New branch for testing the translateX method

--- src/jquery.big-slide.js
+++ src/jquery.big-slide.js
@@ -55,12 +55,23 @@
     };
 
     var animateSlide = {
-      '-webkit-transition': settings.side + ' ' + settings.speed + 'ms ease',
-      '-moz-transition': settings.side + ' ' + settings.speed + 'ms ease',
-      '-ms-transition': settings.side + ' ' + settings.speed + 'ms ease',
-      '-o-transition': settings.side + ' ' + settings.speed + 'ms ease',
-      'transition': settings.side + ' ' + settings.speed + 'ms ease'
+      '-webkit-transition': '-webkit-transform ' + settings.speed + 'ms ease',
+      '-moz-transition': '-moz-transform ' + settings.speed + 'ms ease',
+      '-ms-transition': '-ms-transform ' + settings.speed + 'ms ease',
+      '-o-transition': '-o-transform ' + settings.speed + 'ms ease',
+      'transition': 'transform ' + settings.speed + 'ms ease'
     };
+
+    function translatePosition(elem, val){
+      var args = {
+        '-webkit-transform': 'translateX(' + val + ')',
+        '-moz-transform': 'translateX(' + val + ')',
+        '-ms-transform': 'translateX(' + val + ')',
+        '-o-transform': 'translateX(' + val + ')',
+        'transform': 'translateX(' + val + ')'
+      };
+      elem.css(args);
+    }
 
     menu.css(positionOffScreen);
     menu.css(animateSlide);
@@ -70,14 +81,14 @@
 
     menu.open = function() {
       menu._state = 'open';
-      menu.css(settings.side, '0');
-      push.css(settings.side, width);
+      translatePosition(menu, '0');
+      translatePosition(push, width);
     };
 
     menu.close = function() {
       menu._state = 'closed';
-      menu.css(settings.side, '-' + width);
-      push.css(settings.side, '0');
+      translatePosition(menu, '-' + width);
+      translatePosition(push, '0');
     };
 
     menuLink.on('click.bigSlide', function(e) {
